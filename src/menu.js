b584ca992e4e0176df64a0774f7d4da6c7359ad8
hoholabs <johnpaulhoran@gmail.com>
2022-01-16 19:09:19 -0500

    created function for adding menu sections

--- src/menu.js
+++ src/menu.js
@@ -12,6 +12,66 @@ menuHeader.id = "menu-header";
 
 menuPage.append(menuHeader);
 
+///make menu sections
+
+function makeSection(name){
+const section = document.createElement("section");
+section.className = "menu-section";
+section.id = `${name}-section`;
+
+const sectionHeader = document.createElement("h2");
+sectionHeader.textContent = name;
+section.append(sectionHeader);
+menuPage.appendChild(section);
+}
+
+const breakfastSection = document.createElement("section");
+breakfastSection.className = 'menu-section'
+breakfastSection.id = 'breakfast-section';
+
+const breakfastHeader = document.createElement("h2");
+breakfastHeader.textContent = "Breakfast";
+breakfastSection.append(breakfastHeader);
+
+const breakfastArray = [
+    { name: "eggs",         price: "$1.00",},
+    { name: "toast",        price: "$1.00",},
+    { name: "pancakes",     price: "$1.00",},
+    { name: "french toast", price: "$1.00",},
+    { name: "bacon",        price: "$1.00",},
+    { name: "sausage",      price: "$1.00",},
+    { name: "ham",          price: "$1.00",}
+  ];
+
+const breakfastItems = document.createElement("table");
+
+function itemsTable (array){
+    array.forEach(element => {
+        //create table elements
+        let tbody = document.createElement("tbody");
+        let tr = document.createElement("tr");
+        let tdName = document.createElement("td");
+        tdName.textContent = element.name;
+        let tdPrice = document.createElement("td");
+        tdPrice.textContent = element.price;
+
+        //append elements to table
+        tr.append(tdName);
+        tr.append(tdPrice);
+        tbody.append(tr);
+        breakfastItems.append(tbody);
+    });
+}
+itemsTable(breakfastArray);
+breakfastSection.append(breakfastItems);
+
+menuPage.append(breakfastSection);
+
+
+
+
+let booSection = makeSection("boo");
+
 function loadMenu() {
     content.append(menuPage);
     };
